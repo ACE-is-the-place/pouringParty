e91022d51a66c2da13c7aca7f933c796d08de173
Caity Opelka <caityopelka@gmail.com>
2021-05-20 12:18:12 -0500

    ADD / linter

--- server/index.ts
+++ server/index.ts
@@ -4,14 +4,14 @@ import * as Express from 'express';
 import { resolve } from 'path';
 require('./db/database.ts');
 
-async function bootstrap() {
-  const PORT: String | Number = 8080;
+const bootstrap: any = async () => {
+  const PORT: string | number = 8080;
   const dist = resolve(__dirname, '..', 'dist');
 
   const app = await NestFactory.create(AppModule);
 
-  app.use('/', Express.static(dist));
+  await app.use('/', Express.static(dist));
   await app.listen(<any>PORT, () => console.info(`listening on port http://127.0.0.1:${PORT}`));
-}
+};
 
 bootstrap();
